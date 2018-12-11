fb86b8370a091426a378fc9dfed6205314f68d8b
etubom <way2young4u@hotmail.com>
2018-12-11 23:57:26 +0000

    added handlemath method

--- calc.js
+++ calc.js
@@ -10,16 +10,49 @@ function buttonClick(value) {
   } else {
     handleNumber(value);
   }
+
+  screen.innerText = buffer;
 }
 
-function handleSymbol(symbol) {}
+function handleSymbol(symbol) {
+  //   if (symbol === "C") {
+  //     buffer = "0";
+  //     runningTotal = 0;
+  //   }
+  switch (value) {
+    case "C":
+      buffer = "0";
+      break;
+    case "&plus":
+    case "&minus":
+    case "&times":
+    case "&divide":
+      handleMath(symbol);
+      break;
+  }
+}
+function handleMath(symbol) {
+  if (buffer === "0") {
+    //do nothing
+    return;
+  }
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+
+  buffer = "0";
+}
+
 function handleNumber(numberString) {
   if (buffer === "0") {
     buffer = numberString;
   } else {
     buffer += numberString;
   }
-  screen.innerText = buffer;
 }
 
 function init() {
